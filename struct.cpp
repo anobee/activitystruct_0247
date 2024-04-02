51340d77346eec6e124f3f7e19519aceac52231d
anobee <HP@DESKTOP-KFAVCD8>
2024-04-02 14:02:45 +0700

    first commit

---
+++ struct.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    string umur;
+};
+
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout <<  endl;
+    cout << "n/NIM     :  " <<mhs.nim
+    cout << "n/Nama    :  " <<mhs.nama
+    cout << "n/Alamat  :  " <<mhs.alamat
+    cout << "n/Umur    :  " <<mhs.umur
+
+
+}
